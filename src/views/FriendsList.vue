e7b31ae392d15eb9ae2d9cea3260c98e0d78efd6
Pol Curto <pol.curto@students.salle.url.edu>
2022-10-21 11:04:32 +0200

    firends list en proceso

--- src/views/FriendsList.vue
+++ src/views/FriendsList.vue
@@ -1,7 +1,39 @@
 <template>
     <main>
-      <header>
-        <h3>Friends list</h3>
-      </header>
+
+      <div class="topText">
+        <h1>Friends list</h1>
+      </div>
+      
+      <section class="grid-container">
+        <div>
+          <img src='https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png' class='imgList'/>
+          Friend 1
+          <button>View profile</button>  
+        </div>
+        <div>
+          <img src='https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png' class='imgList'/> Friend 2
+        </div>
+        <div>
+          <img src='https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png' class='imgList'/> Friend 3
+        </div>
+        <div>
+          <img src='https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png' class='imgList'/> Friend 4
+        </div>
+        <div>
+          <img src='https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png' class='imgList'/> Friend 5
+        </div>
+        <div>
+          <img src='https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png' class='imgList'/> Friend 6
+        </div>
+        <div>
+          <img src='https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png' class='imgList'/> Friend 7
+        </div>
+        <div>
+          <img src='https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png' class='imgList'/> Friend 8
+        </div>
+      </section>
+   
+
     </main>
   </template>
