40bad470982ed4f4070aa33c0c6ead6282b2789b
Pol Curto <pol.curto@students.salle.url.edu>
2022-10-23 12:48:50 +0200

    Pending requests adaptat (una mica pocho)

--- src/views/FriendsList.vue
+++ src/views/FriendsList.vue
@@ -15,42 +15,42 @@
         <div>
           <img src='https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png' class='imgList'/>
           Friend 1
-          <button>View profile</button>  
+          <button>Profile</button>  
         </div>
         <div>
           <img src='https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png' class='imgList'/> 
           Friend 2
-          <button>View profile</button>  
+          <button>Profile</button>  
         </div>
         <div>
           <img src='https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png' class='imgList'/>
           Friend 3
-          <button>View profile</button>  
+          <button>Profile</button>  
         </div>
         <div>
           <img src='https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png' class='imgList'/>
           Friend 4
-          <button>View profile</button>  
+          <button>Profile</button>  
         </div>
         <div>
           <img src='https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png' class='imgList'/> 
           Friend 5
-          <button>View profile</button>  
+          <button>Profile</button>  
         </div>
         <div>
           <img src='https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png' class='imgList'/> 
           Friend 6
-          <button>View profile</button>  
+          <button>Profile</button>  
         </div>
         <div>
           <img src='https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png' class='imgList'/> 
           Friend 7
-          <button>View profile</button>  
+          <button>Profile</button>  
         </div>
         <div>
           <img src='https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png' class='imgList'/> 
           Friend 8
-          <button>View profile</button>  
+          <button>Profile</button>  
         </div>
       </div>
    
--- src/views/PendingRequests.vue
+++ src/views/PendingRequests.vue
@@ -1,13 +1,16 @@
 <template>
     <main>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
         <div class="topText">
             <h1>Pending Requests</h1>
         </div>
         <br/><br/>
-        <div>
-            <input class="searchBar" placeholder="Search person..."><button>Search</button><br/><br/><br/>
+        <div class="searchContainer">
+            <input type="text" placeholder="Search friend...">
+            <button type="submit"><i class="fa fa-search"></i></button><br/><br/>
         </div>
+        <br/>
       
         <section class="grid-container">
             <div>
@@ -15,7 +18,7 @@
             Person 1
             <button>Deny</button>   
             <button>Accept</button>
-            <button>View profile</button>  
+            <button>Profile</button>  
             
             </div>
             <div>
@@ -23,35 +26,35 @@
             Person 2
             <button>Deny</button>   
             <button>Accept</button>
-            <button>View profile</button>  
+            <button>Profile</button>  
             </div>
             <div>
             <img src='https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png' class='imgList'/>
             Person 3
             <button>Deny</button>   
             <button>Accept</button>
-            <button>View profile</button>  
+            <button>Profile</button>  
             </div>
             <div>
             <img src='https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png' class='imgList'/>
             Person 4
             <button>Deny</button>   
             <button>Accept</button>
-            <button>View profile</button>  
+            <button>Profile</button>  
             </div>
             <div>
             <img src='https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png' class='imgList'/> 
             Person 5
             <button>Deny</button>   
             <button>Accept</button>
-            <button>View profile</button>  
+            <button>Profile</button>  
             </div>
             <div>
             <img src='https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png' class='imgList'/> 
             Person 6
             <button>Deny</button>   
             <button>Accept</button>
-            <button>View profile</button>  
+            <button>Profile</button>  
 
             </div>
             <div>
@@ -59,14 +62,14 @@
             Person 7
             <button>Deny</button>   
             <button>Accept</button>
-            <button>View profile</button>  
+            <button>Profile</button>  
             </div>
             <div>
             <img src='https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png' class='imgList'/> 
             Person 8
             <button>Deny</button>   
             <button>Accept</button>
-            <button>View profile</button>  
+            <button>Profile</button>  
             </div>
         </section>
    
