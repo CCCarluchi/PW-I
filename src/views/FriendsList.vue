0338433858b2f654a868397f7c0caf0ac5396430
Pol Curto <pol.curto@students.salle.url.edu>
2022-10-23 12:26:40 +0200

    Friend list adaptada

--- src/views/FriendsList.vue
+++ src/views/FriendsList.vue
@@ -1,15 +1,17 @@
 <template>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <main>
 
       <div class="topText">
         <h1>Friends list</h1>
       </div>
       <br/><br/>
-        <div>
-            <input class="searchBar" placeholder="Search friend..."><button>Search</button><br/><br/><br/>
+        <div class="searchContainer">
+            <input type="text" placeholder="Search friend...">
+            <button type="submit"><i class="fa fa-search"></i></button><br/><br/>
         </div>
-      
-      <section class="grid-container">
+        <br/>
+      <div class="grid-container">
         <div>
           <img src='https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png' class='imgList'/>
           Friend 1
@@ -50,7 +52,7 @@
           Friend 8
           <button>View profile</button>  
         </div>
-      </section>
+      </div>
    
 
     </main>
