768c32915ddbf744b890194a86375c3d1efa43e1
Carla Collado <c.collado@students.salle.url.edu>
2022-10-26 13:59:34 +0200

    Avance en SignIn, SignOut y SingUp + style.

--- src/views/SignOut.vue
+++ src/views/SignOut.vue
@@ -1,5 +1,19 @@
 <template>
   <main>
-    <h1>Sign Out</h1>
+    <div> 
+    <i class="arrow left"></i>
+    </div>
+    <div type="card">
+       <div class="appTitle">
+       <h2>Sign out</h2>
+       </div>
+       <div align="left">
+       <p2>Do you wish to sign out?</p2>
+       </div>
+       <br/><br/>
+       <div class="inputContainer">
+       <button>Sign out</button>
+       </div>
+    </div>
   </main>
 </template>
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,8 +1,45 @@
 <template>
   <main>
-    <article>
-      <h1>Sign Up</h1>
-      <p> <i class="arrow left"></i></p>
-    </article>
+    <div> 
+    <i class="arrow left"></i>
+    </div>
+    <div class="appTitle">
+    <h2>Sign up</h2>
+    </div>
+    <div class="inputContainer">
+    <input type="text" placeholder="*Name"><br/>
+    </div>
+    <br/><br/>
+    <div class="inputContainer">
+    <input type="text" placeholder="*Surname"><br/>
+    </div>
+    <br/><br/>
+    <div class="inputContainer">
+    <input type="text" placeholder="*Username"><br/>
+    </div>
+    <br/><br/>
+    <div class="inputContainer">
+    <input type="date" placeholder="*Date of birth"><br/>
+    </div>
+    <br/><br/>
+    <div class="inputContainer">
+    <input type="text" placeholder="*Email"><br/>
+    </div>
+    <br/><br/>
+    <div class="inputContainer">
+    <input type="text" placeholder="*Password"><br/>
+    </div>
+    <br/><br/>
+    <div class="inputContainer">
+    <input type="text" placeholder="*Confirm Password"><br/>
+    </div>
+    <br/><br/>
+    <div class="inputContainer">
+    <input type="text" placeholder="+ Add your profile picture"><br/>
+    </div>
+    <br/><br/>
+    <div class="inputContainer">
+    <button>Register</button>
+    </div>
   </main>
 </template>
