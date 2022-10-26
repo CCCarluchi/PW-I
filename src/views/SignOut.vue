df20d1e1654aace7a37dbd17173e1c6aae786208
Carla Collado <c.collado@students.salle.url.edu>
2022-10-26 15:16:59 +0200

    Centrar card

--- src/views/SignOut.vue
+++ src/views/SignOut.vue
@@ -3,17 +3,16 @@
     <div> 
     <i class="arrow left"></i>
     </div>
-    <div type="card">
-       <div class="appTitle">
+    <br/>
+    <div class="card">
+       <div align = "left">
        <h2>Sign out</h2>
        </div>
-       <div align="left">
+       <div align = "left">
        <p2>Do you wish to sign out?</p2>
        </div>
        <br/><br/>
-       <div class="inputContainer">
        <button>Sign out</button>
-       </div>
     </div>
   </main>
 </template>
