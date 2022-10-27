b4e1b74c0c940742b1b6f8ffa5a403417a45bc68
Carla Collado <c.collado@students.salle.url.edu>
2022-10-27 14:03:21 +0200

    Estructura events y create events + update style.

--- src/views/CreateEvent.vue
+++ src/views/CreateEvent.vue
@@ -1,5 +1,34 @@
 <template>
   <main>
-    <h1>Create event</h1>
+    <br/>
+    <div class="arrowContainer" align="left"> 
+    <i class="arrow left"></i>
+    </div>
+    <div class="appTitle">
+    <h2>Create event</h2>
+    </div>
+    <div class="inputContainer">
+    <input type="text" placeholder="*Name"><br/>
+    </div>
+    <br/><br/>
+    <div class="inputContainer">
+    <input type="date" placeholder="*Start date"><br/>
+    </div>
+    <br/><br/>
+    <div class="inputContainer">
+    <input type="date" placeholder="*End date"><br/>
+    </div>
+    <br/><br/>
+    <div class="inputContainer">
+    <input type="text" placeholder="*Location"><br/>
+    </div>
+    <br/><br/>
+    <div class="inputContainer">
+    <textarea type="text" placeholder="*Details"></textarea><br/>
+    </div>
+    <br/><br/>
+    <div class="inputContainer">
+    <button>Create</button>
+    </div>
   </main>
 </template>
