7f2afa1a139a8ee83480f0f8671705161ef87fef
pablo <pablo.gfuentes@students.salle.url.edu>
2022-10-27 18:20:23 +0200

    Delete account

--- src/views/DeleteAccount.vue
+++ src/views/DeleteAccount.vue
@@ -1,5 +1,19 @@
 <template>
-    <main>
-       
-    </main>
-  </template>
+  <main>
+    <br/>
+    <div class="arrowContainer" align="left"> 
+    <i class="arrow left"></i>
+    </div>
+    <br/>
+    <div class="card">
+       <div align = "left">
+       <h2>Delete account</h2>
+       </div>
+       <div align = "left">
+       <p2>Do you wish to delete account?</p2>
+       </div>
+       <br/><br/>
+       <button>Delete it</button>
+    </div>
+  </main>
+</template>
