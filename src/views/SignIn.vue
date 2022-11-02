31a6d01fd6f466546addb009340a3dde20ff7a5b
Carla Collado <c.collado@students.salle.url.edu>
2022-11-02 10:46:17 +0100

    Estructurar codi

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,25 +1,35 @@
 <template>
   <main>
-    <div class="appTitle" >
-    <h1>OpenEvents</h1>
-    </div>
-    <img src="https://cdn.logo.com/hotlink-ok/logo-social-sq.png" alt="logo" class="imgLogo">
+    <header class="appTitle">
+        <h1>OpenEvents</h1>
+    </header>
+
+    <figure>
+      <img src="https://cdn.logo.com/hotlink-ok/logo-social-sq.png" alt="logo" class="imgLogo">
+    </figure>
     <br/><br/>
-    <div class="inputContainer">
-    <input type="text" placeholder="Username"><br/>
-    </div>
+
+    <article>
+      <div class="inputContainer">
+        <input type="text" placeholder="Username"><br/>
+      </div>
+      <br/><br/>
+      <div class="inputContainer">
+        <input type="password" placeholder="Password"><br/>
+      </div>
+      <br/><br/>
+      <div class="inputContainer">
+        <router-link to="/Home" id="button"><button>Sign in</button></router-link>
+      </div>
+      <br/><br/>
+      <div class="inputContainer">
+        <router-link to="/SignUp" id="a2"><a2>Sign up</a2></router-link>
+      </div>
+    </article>
     <br/><br/>
-    <div class="inputContainer">
-    <input type="password" placeholder="Password"><br/>
-    </div>
-    <br/><br/>
-    <div class="inputContainer">
-    <router-link to="/Home" id="button"><button>Sign in</button></router-link>
-    </div>
-    <br/><br/>
-    <div class="inputContainer">
-    <router-link to="/SignUp" id="a2"><a2>Sign up</a2></router-link>
-    </div>
-    <br/><br/>
+
+    <footer>
+    </footer>
+
   </main>
 </template>
