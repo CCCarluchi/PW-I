9b7132af1b5d57539cc05111c636e79e31692e53
pablo <pablo.gfuentes@students.salle.url.edu>
2022-11-09 15:48:33 +0100

    No favourite eventes

--- src/views/Data.vue
+++ src/views/Data.vue
@@ -9,7 +9,6 @@
         <img src='https://i.blogs.es/80e441/big-chungus-multiversus/1366_2000.webp' class='imgRedonda' /><br/><br/>
       <h1>Name</h1>
       <router-link to="/Statistics" id="button"><button2>Public Stats</button2></router-link><br/><br/>
-      <button2 type="button">Favourite events</button2><br/><br/>
       <router-link to="/ModifyData" id="button"><button2>Change profile</button2></router-link><br/><br/>
       <router-link to="/SignOut" id="button"><button2>Sign Out</button2></router-link><br/><br/>
       <router-link to="/DeleteAccount" id="button"><button2>Delete Account</button2></router-link><br/><br/>
