0927a7c39b20ac38b152cbe4b57ec26ba28a0d6a
Carla Collado <c.collado@students.salle.url.edu>
2022-11-11 11:50:57 +0100

    Botones event

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -10,13 +10,13 @@
       </div>
     </header>
 
-    <article>
+    <article class="column">
         <div class="appText">
             <p1>Name</p1><br/>
         </div>
 
         <div class="appText2">
-            <p2>Name</p2><br/><br/>
+            <p2>EventName</p2><br/><br/>
         </div>
 
         <div class="appText">
@@ -24,7 +24,7 @@
         </div>
 
         <div class="appText2">
-            <p2>Start date</p2><br/><br/>
+            <p2>XX/XX/XXXX</p2><br/><br/>
         </div>
 
         <div class="appText">
@@ -32,7 +32,7 @@
         </div>
 
         <div class="appText2">
-            <p2>End date</p2><br/><br/>
+            <p2>XX/XX/XXXX</p2><br/><br/>
         </div>
 
         <div class="appText">
@@ -40,7 +40,7 @@
         </div>
 
         <div class="appText2">
-            <p2>Location</p2><br/><br/>
+            <p2>EventLocation</p2><br/><br/>
         </div>
 
         <div class="appText">
@@ -48,10 +48,21 @@
         </div>
 
         <div class="appText2">
-            <p2>Details</p2><br/><br/>
+            <p2>EventDetails</p2><br/><br/>
         </div>
     </article>
     <br/><br/>
+
+    <nav class="column">
+        <div class="inputContainer">
+        <router-link to="/Share" id="button"><button>Share</button></router-link>
+        </div>
+        <br/>
+
+        <div class="inputContainer">
+        <router-link to="/ParticipateEvent" id="button"><button>Participate</button></router-link>
+        </div>
+    </nav>
 
     <footer>
     </footer>
