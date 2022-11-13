b2e9cb9eeac2b9fb78255093a44658563e3be28a
DESKTOP-UE8F1HT\clatv <claudiaximena.perez@students.salle.url.edu>
2022-11-13 12:53:21 +0100

    Timeline

--- src/views/Timeline.vue
+++ src/views/Timeline.vue
@@ -1,19 +1,54 @@
 <template>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <main>
-    <header>
+      <header>
         <br/>
         <div class="arrowContainer" align="left"> 
-            <router-link to="/Events" id="i"><i class="arrow left"></i></router-link>
+          <router-link to="/Events" id="i"><i class="arrow left"></i></router-link>
         </div>
         <div class="searchTitle">
-        <h2>Timeline</h2>
-      </div>
-    </header>
-   
-    <article>
-        
-    </article>
+          <h2>Timeline</h2>
+        </div>
+      </header>
+  
+      <nav>
+        <div class="listContainer2" align="left">
+          <img src="https://media.istockphoto.com/photos/purple-defocused-blurred-motion-abstract-background-picture-id1273929462?b=1&k=20&m=1273929462&s=612x612&w=0&h=SiJ-IIlRT_GMs_UGFMaMcHhIosKvvvMnb0PNE3wGk6k=" class="imgList2"/>
+          <router-link to="/Event" id="p"><p1>Event1</p1><p2>  XX/XX/XXXX  Location</p2></router-link>
+        </div>
+        <br/>
+  
+        <div class="listContainer2" align="left">
+          <img src="https://media.istockphoto.com/photos/purple-defocused-blurred-motion-abstract-background-picture-id1273929462?b=1&k=20&m=1273929462&s=612x612&w=0&h=SiJ-IIlRT_GMs_UGFMaMcHhIosKvvvMnb0PNE3wGk6k=" class="imgList2"/>
+          <router-link to="/Event" id="p"><p1>Event2</p1><p2>  XX/XX/XXXX  Location</p2></router-link>
+        </div>
+        <br/>
+  
+        <div class="listContainer2" align="left">
+          <img src="https://media.istockphoto.com/photos/purple-defocused-blurred-motion-abstract-background-picture-id1273929462?b=1&k=20&m=1273929462&s=612x612&w=0&h=SiJ-IIlRT_GMs_UGFMaMcHhIosKvvvMnb0PNE3wGk6k=" class="imgList2"/>
+          <router-link to="/Event" id="p"><p1>Event3</p1><p2>  XX/XX/XXXX  Location</p2></router-link>
+        </div>
+        <br/>
+  
+        <div class="listContainer2" align="left">
+          <img src="https://media.istockphoto.com/photos/purple-defocused-blurred-motion-abstract-background-picture-id1273929462?b=1&k=20&m=1273929462&s=612x612&w=0&h=SiJ-IIlRT_GMs_UGFMaMcHhIosKvvvMnb0PNE3wGk6k=" class="imgList2"/>
+          <router-link to="/Event" id="p"><p1>Event4</p1><p2>  XX/XX/XXXX  Location</p2></router-link>
+        </div>
+        <br/>
+  
+        <div class="listContainer2" align="left">
+          <img src="https://media.istockphoto.com/photos/purple-defocused-blurred-motion-abstract-background-picture-id1273929462?b=1&k=20&m=1273929462&s=612x612&w=0&h=SiJ-IIlRT_GMs_UGFMaMcHhIosKvvvMnb0PNE3wGk6k=" class="imgList2"/>
+          <router-link to="/Event" id="p"><p1>Event5</p1><p2>  XX/XX/XXXX  Location</p2></router-link>
+        </div>
+        <br/>
+  
+        <div class="listContainer2" align="left">
+          <img src="https://media.istockphoto.com/photos/purple-defocused-blurred-motion-abstract-background-picture-id1273929462?b=1&k=20&m=1273929462&s=612x612&w=0&h=SiJ-IIlRT_GMs_UGFMaMcHhIosKvvvMnb0PNE3wGk6k=" class="imgList2"/>
+          <router-link to="/Event" id="p"><p1>Event6</p1><p2>  XX/XX/XXXX  Location</p2></router-link>
+        </div>
+      </nav>
+      <br/><br/>
+  
+      <footer>
+      </footer>
     </main>
-
-</template>
+  </template>
