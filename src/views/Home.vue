dd692698a8ea24e9e406caa84cc3329f9d9d57ff
Pol Curto <pol.curto@students.salle.url.edu>
2022-11-13 17:26:45 +0100

    home be

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
     <main>
     <header class="appTitle">
-        <h2>Home</h2>
+        <h1>Home</h1>
     </header>
 
     <nav>
         <div class="buttonHome">
-            <router-link to="/Data" id="button"><button class="buttonH">Profile</button></router-link>
-        
-            <router-link to="/FriendsList" id="button"><button class="buttonH">Friends</button></router-link>
-        
-            <router-link to="/Events" id="button"><button class="buttonH">Events</button></router-link>
-    
-            <router-link to="/Chat" id="button"><button class="buttonH">Chat</button></router-link>
+            <div class="homeItemLeft">
+                <router-link to="/Data" id="button"><button class="buttonH">Profile</button></router-link>
+            </div>
+            <div class="homeItemRight">
+                <router-link to="/FriendsList" id="button"><button class="buttonH">Friends</button></router-link>
+            </div>
+            <div class="homeItemLeft">
+                <router-link to="/Events" id="button"><button class="buttonH">Events</button></router-link>
+            </div>
+            <div class="homeItemRight">
+                <router-link to="/Chat" id="button"><button class="buttonH">Chat</button></router-link>
+            </div>
         </div>
     </nav>
 
