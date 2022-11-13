d081756d26dd757a3f7885c2acf110dca1178961
Pol Curto <pol.curto@students.salle.url.edu>
2022-11-13 20:53:40 +0100

    timeline be

--- src/views/Timeline.vue
+++ src/views/Timeline.vue
@@ -2,49 +2,49 @@
     <main>
       <header>
         <br/>
-        <div class="arrowContainer" align="left"> 
-          <router-link to="/Events" id="i"><i class="arrow left"></i></router-link>
-        </div>
-        <div class="appTitle">
-          <h2>Timeline</h2>
+        <router-link to="/Home" id="i"><i class="arrow left"></i></router-link>
+        <br/><br/>
+        <div class="topText">
+          <h1>Timeline</h1>
         </div>
       </header>
   
       <nav>
-        <div class="listContainer2" align="left">
-          <img src="https://images.squarespace-cdn.com/content/v1/5a726fcd4c0dbfa531ec98cf/1523669284136-BTJA038C00QFP5M3AX0N/image-asset.jpeg" class="imgList2"/>
-          <router-link to="/Event" id="p"><p1>Event1</p1><p2>  XX/XX/XXXX  Location</p2></router-link>
-        </div>
-        <br/>
-  
-        <div class="listContainer2" align="left">
-          <img src="https://images.squarespace-cdn.com/content/v1/5a726fcd4c0dbfa531ec98cf/1523669284136-BTJA038C00QFP5M3AX0N/image-asset.jpeg" class="imgList2"/>
-          <router-link to="/Event" id="p"><p1>Event2</p1><p2>  XX/XX/XXXX  Location</p2></router-link>
-        </div>
-        <br/>
-  
-        <div class="listContainer2" align="left">
-          <img src="https://images.squarespace-cdn.com/content/v1/5a726fcd4c0dbfa531ec98cf/1523669284136-BTJA038C00QFP5M3AX0N/image-asset.jpeg" class="imgList2"/>
-          <router-link to="/Event" id="p"><p1>Event3</p1><p2>  XX/XX/XXXX  Location</p2></router-link>
-        </div>
-        <br/>
-  
-        <div class="listContainer2" align="left">
-          <img src="https://images.squarespace-cdn.com/content/v1/5a726fcd4c0dbfa531ec98cf/1523669284136-BTJA038C00QFP5M3AX0N/image-asset.jpeg" class="imgList2"/>
-          <router-link to="/Event" id="p"><p1>Event4</p1><p2>  XX/XX/XXXX  Location</p2></router-link>
-        </div>
-        <br/>
-  
-        <div class="listContainer2" align="left">
-          <img src="https://images.squarespace-cdn.com/content/v1/5a726fcd4c0dbfa531ec98cf/1523669284136-BTJA038C00QFP5M3AX0N/image-asset.jpeg" class="imgList2"/>
-          <router-link to="/Event" id="p"><p1>Event5</p1><p2>  XX/XX/XXXX  Location</p2></router-link>
-        </div>
-        <br/>
-  
-        <div class="listContainer2" align="left">
-          <img src="https://images.squarespace-cdn.com/content/v1/5a726fcd4c0dbfa531ec98cf/1523669284136-BTJA038C00QFP5M3AX0N/image-asset.jpeg" class="imgList2"/>
-          <router-link to="/Event" id="p"><p1>Event6</p1><p2>  XX/XX/XXXX  Location</p2></router-link>
-        </div>
+        <br/><br/>
+          <div class="listContainer2">
+            <img src="https://images.squarespace-cdn.com/content/v1/5a726fcd4c0dbfa531ec98cf/1523669284136-BTJA038C00QFP5M3AX0N/image-asset.jpeg" class="imgList2"/>
+            <router-link to="/Event" id="p"><p1>Event1</p1><p2>  XX/XX/XXXX  Location</p2></router-link>
+          </div>
+          <br/><br/><br/><br/>
+    
+          <div class="listContainer2">
+            <img src="https://images.squarespace-cdn.com/content/v1/5a726fcd4c0dbfa531ec98cf/1523669284136-BTJA038C00QFP5M3AX0N/image-asset.jpeg" class="imgList2"/>
+            <router-link to="/Event" id="p"><p1>Event2</p1><p2>  XX/XX/XXXX  Location</p2></router-link>
+          </div>
+          <br/><br/><br/><br/>
+    
+          <div class="listContainer2">
+            <img src="https://images.squarespace-cdn.com/content/v1/5a726fcd4c0dbfa531ec98cf/1523669284136-BTJA038C00QFP5M3AX0N/image-asset.jpeg" class="imgList2"/>
+            <router-link to="/Event" id="p"><p1>Event3</p1><p2>  XX/XX/XXXX  Location</p2></router-link>
+          </div>
+          <br/><br/><br/><br/>
+    
+          <div class="listContainer2">
+            <img src="https://images.squarespace-cdn.com/content/v1/5a726fcd4c0dbfa531ec98cf/1523669284136-BTJA038C00QFP5M3AX0N/image-asset.jpeg" class="imgList2"/>
+            <router-link to="/Event" id="p"><p1>Event4</p1><p2>  XX/XX/XXXX  Location</p2></router-link>
+          </div>
+          <br/><br/><br/><br/>
+    
+          <div class="listContainer2">
+            <img src="https://images.squarespace-cdn.com/content/v1/5a726fcd4c0dbfa531ec98cf/1523669284136-BTJA038C00QFP5M3AX0N/image-asset.jpeg" class="imgList2"/>
+            <router-link to="/Event" id="p"><p1>Event5</p1><p2>  XX/XX/XXXX  Location</p2></router-link>
+          </div>
+          <br/><br/><br/><br/>
+    
+          <div class="listContainer2">
+            <img src="https://images.squarespace-cdn.com/content/v1/5a726fcd4c0dbfa531ec98cf/1523669284136-BTJA038C00QFP5M3AX0N/image-asset.jpeg" class="imgList2"/>
+            <router-link to="/Event" id="p"><p1>Event6</p1><p2>  XX/XX/XXXX  Location</p2></router-link>
+          </div>
       </nav>
       <br/><br/>
   
