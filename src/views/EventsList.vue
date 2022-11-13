3ef393707390bd4e4fc2d5536cecde7673120236
Pol Curto <pol.curto@students.salle.url.edu>
2022-11-13 21:41:35 +0100

    arreglat

--- src/views/EventsList.vue
+++ src/views/EventsList.vue
@@ -36,37 +36,37 @@
       </div>
       <br/>
 
-      <div class="listContainer2" align="left">
+      <div class="listContainer3" align="left">
         <img src="https://images.squarespace-cdn.com/content/v1/5a726fcd4c0dbfa531ec98cf/1523669284136-BTJA038C00QFP5M3AX0N/image-asset.jpeg" class="imgList2"/>
         <router-link to="/Event" id="p"><p1>Event1</p1><p2>  XX/XX/XXXX  Location</p2></router-link>
       </div>
       <br/>
 
-      <div class="listContainer2" align="left">
+      <div class="listContainer3" align="left">
         <img src="https://images.squarespace-cdn.com/content/v1/5a726fcd4c0dbfa531ec98cf/1523669284136-BTJA038C00QFP5M3AX0N/image-asset.jpeg" class="imgList2"/>
         <router-link to="/Event" id="p"><p1>Event2</p1><p2>  XX/XX/XXXX  Location</p2></router-link>
       </div>
       <br/>
 
-      <div class="listContainer2" align="left">
+      <div class="listContainer3" align="left">
         <img src="https://images.squarespace-cdn.com/content/v1/5a726fcd4c0dbfa531ec98cf/1523669284136-BTJA038C00QFP5M3AX0N/image-asset.jpeg" class="imgList2"/>
         <router-link to="/Event" id="p"><p1>Event3</p1><p2>  XX/XX/XXXX  Location</p2></router-link>
       </div>
       <br/>
 
-      <div class="listContainer2" align="left">
+      <div class="listContainer3" align="left">
         <img src="https://images.squarespace-cdn.com/content/v1/5a726fcd4c0dbfa531ec98cf/1523669284136-BTJA038C00QFP5M3AX0N/image-asset.jpeg" class="imgList2"/>
         <router-link to="/Event" id="p"><p1>Event4</p1><p2>  XX/XX/XXXX  Location</p2></router-link>
       </div>
       <br/>
 
-      <div class="listContainer2" align="left">
+      <div class="listContainer3" align="left">
         <img src="https://images.squarespace-cdn.com/content/v1/5a726fcd4c0dbfa531ec98cf/1523669284136-BTJA038C00QFP5M3AX0N/image-asset.jpeg" class="imgList2"/>
         <router-link to="/Event" id="p"><p1>Event5</p1><p2>  XX/XX/XXXX  Location</p2></router-link>
       </div>
       <br/>
 
-      <div class="listContainer2" align="left">
+      <div class="listContainer3" align="left">
         <img src="https://images.squarespace-cdn.com/content/v1/5a726fcd4c0dbfa531ec98cf/1523669284136-BTJA038C00QFP5M3AX0N/image-asset.jpeg" class="imgList2"/>
         <router-link to="/Event" id="p"><p1>Event6</p1><p2>  XX/XX/XXXX  Location</p2></router-link>
       </div>
