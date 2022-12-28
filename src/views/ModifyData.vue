bf6148f1ed0cbfe4eccfc509b8597ce5325efe7d
pablo <pablo.gfuentes@students.salle.url.edu>
2022-12-28 21:39:43 +0100

    botton modify foto

--- src/views/ModifyData.vue
+++ src/views/ModifyData.vue
@@ -1,3 +1,10 @@
+<script>
+
+
+
+</script>
+
+
 <template>
 
     <main>
@@ -29,13 +36,35 @@
         </div>
      <br/><br/>
         <img src='https://pbs.twimg.com/media/EztG5y0WQAAPS69?format=jpg&name=medium' class='imgRedonda' /><br/><br/>
-        <button class = "button2">Change picture</button><br/><br/>
+        <input type="file" id="actual-btn" hidden/>
+        <label class = "labeel" for="actual-btn">Change picture</label><br/><br/>
+        <span id="file-chosen">No file chosen</span>
+        
         <br/><br/>
         <router-link to="/Data" id="button2"><button class = "button2">Apply Changes</button></router-link><br/><br/>
       </article>  
+      
+      <input style="display:none" type="file" id="my-file">
+<button type="button" onclick="document.getElementById('my-file').click()">Choose file\</button>
 
       <footer>
       </footer>
       
     </main>
   </template>
+
+<style>
+.labeel {
+  background-color: indigo;
+  color: white;
+  padding: 0.5rem;
+  font-family: sans-serif;
+  border-radius: 0.3rem;
+  cursor: pointer;
+  margin-top: 1rem;
+}
+#file-chosen{
+  margin-left: 0.3rem;
+  font-family: sans-serif;
+}
+</style>
