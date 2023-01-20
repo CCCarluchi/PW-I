9c1927e988cc1a31cf48d4c54de8d1370a032944
Pol Curto <pol.curto@students.salle.url.edu>
2023-01-20 21:16:41 +0100

    Root component

--- src/components/Bar.vue
+++ src/components/Bar.vue
@@ -21,3 +21,4 @@ export default {
   <BackArrow v-on:back="back"></BackArrow>
   <HomeButton v-on:home="home"></HomeButton>
 </template>
+
--- src/components/HomeButton.vue
+++ src/components/HomeButton.vue
@@ -1,3 +1,22 @@
+<script>
+export default {
+    emits: ['home']
+}
+</script>
+
 <template>
-    <button v-on:click="$emit('home')"><i class="fa fa-home"></i></button>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <button class="home" v-on:click="$emit('home')"><i class="fa fa-home"></i></button>
 </template>
+
+<style>
+    .home {
+        margin-left: 10px;
+        float: left;
+        font-size: 14px;
+        width: 20px;
+        height: 20px;
+        color:black;
+        background-color: white;
+    }
+</style>
